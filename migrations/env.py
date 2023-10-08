d4635f8d3aa2b75cdea0b19011161b4b1ea20262
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-10-08 18:21:56 +0200

    full operations/ its work

--- migrations/env.py
+++ migrations/env.py
@@ -34,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata_auth, metadata_operations]
+target_metadata = {metadata_auth, metadata_operations}
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
