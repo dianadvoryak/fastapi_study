48cf38bebfa82efd2be502dd572985370b047e80
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-10-08 14:10:42 +0200

    add dotenv

--- src/config.py
+++ src/config.py
@@ -1,1 +1,12 @@
-SECRET = "e95a3684b9982fcfd46eea716707f80cef515906eb49c4cb961dfde39a41ce21"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
+SECRET_AUTH = os.environ.get("SECRET_AUTH")
