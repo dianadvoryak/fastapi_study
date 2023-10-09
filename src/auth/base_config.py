7130f80af4abaaab3ff9f09190f0d9c12f1f0360
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-10-09 20:23:28 +0200

    add Exception

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -3,12 +3,12 @@ from fastapi_users.authentication import JWTStrategy
 from fastapi_users import FastAPIUsers
 from auth.manager import get_user_manager
 from auth.models import User
-from config import SECRET
+from config import SECRET_AUTH
 
 cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=3600)
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET_AUTH, lifetime_seconds=3600)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
--- src/operations/router.py
+++ src/operations/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
@@ -13,11 +13,23 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[OperationCreate])
+@router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    result = await session.execute(query)
-    return result.all()
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        return {
+            "status": "success",
+            "data": result.mappings().all(),
+            "details": None
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
+
 
 @router.post("/")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
