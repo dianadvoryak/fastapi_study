4968ae6d58351c3b5376909be033fd1e16c46e9e
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-10-22 10:15:11 +0200

    add prepend_sys_path in alembic.ini

--- migrations/env.py
+++ migrations/env.py
@@ -12,9 +12,9 @@ sys.path.append(os.path.join(sys.path[0], 'src'))
 # sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 # sys.path.append(os.getcwd())
 
-from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from src.auth.models import metadata as metadata_auth
-from src.operations.models import metadata as metadata_operations
+from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from auth.models import metadata as metadata_auth
+from operations.models import metadata as metadata_operations
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
