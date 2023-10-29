1fe222e4ebb10c34992b6b3d7ed2b7a2764241fd
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-10-29 17:06:00 +0200

    TEST WORK!

--- src/operations/router.py
+++ src/operations/router.py
@@ -37,9 +37,14 @@ async def get_specific_operations(operation_type: str, session: AsyncSession = D
         })
 
 
-@router.post("/")
+@router.post("")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
     stmt = insert(operation).values(**new_operation.model_dump())
     await session.execute(stmt)
     await session.commit()
     return {"status": "success"}
+
+@router.get("/main")
+async def main(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(1))
+    return result.all()
